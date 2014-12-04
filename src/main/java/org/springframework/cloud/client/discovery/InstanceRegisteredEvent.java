9a73f65129ef4f8fa928c54fdb7af1c33d7f50cc
Spencer Gibb <spencer@gibb.us>
2014-12-04 11:54:01 -0700

    Polish

--- src/main/java/org/springframework/cloud/client/discovery/InstanceRegisteredEvent.java
+++ src/main/java/org/springframework/cloud/client/discovery/InstanceRegisteredEvent.java
@@ -3,6 +3,7 @@ package org.springframework.cloud.client.discovery;
 import org.springframework.context.ApplicationEvent;
 
 /**
+ * Event to be published after the local service instance registers itself with a discovery service
  * @author Spencer Gibb
  */
 @SuppressWarnings("serial")
@@ -10,8 +11,6 @@ public class InstanceRegisteredEvent<T> extends ApplicationEvent {
     private T config;
 
     /**
-     * Create a new ApplicationEvent.
-     *
      * @param source the component that published the event (never {@code null})
      * @param config the configuration of the instance
      */
