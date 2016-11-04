81993a43b21cb4d4a3898ab2c04c841fc60eb26d
Spencer Gibb <spencer@gibb.us>
2016-11-04 16:58:20 -0600

    Adds AutoServiceRegistrationProperties

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationConfiguration.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationConfiguration.java
@@ -1,6 +1,7 @@
 package org.springframework.cloud.client.serviceregistry;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -9,15 +10,18 @@ import javax.annotation.PostConstruct;
  * @author Spencer Gibb
  */
 @Configuration
+@EnableConfigurationProperties(AutoServiceRegistrationProperties.class)
 public class AutoServiceRegistrationConfiguration {
 
 	@Autowired(required = false)
 	private AutoServiceRegistration autoServiceRegistration;
 
+	@Autowired
+	private AutoServiceRegistrationProperties properties;
+
 	@PostConstruct
 	protected void init() {
-		boolean failFast = false; // TODO: move to configuration props
-		if (autoServiceRegistration == null && failFast) {
+		if (autoServiceRegistration == null && this.properties.isFailFast()) {
 			throw new IllegalStateException("Auto Service Registration has been requested, but there is no AutoServiceRegistration bean");
 		}
 	}
