f7e59a4bfec365af2260e72e45d425a59c450e9e
Spencer Gibb <spencer@gibb.us>
2016-12-08 09:52:33 -0700

    Change auto-registration intitialization.
    The import-selected AutoServiceRegistrationConfiguration now creates the configuration properties bean, which is used as a marker bean for AutoServiceRegistrationAutoConfiguration. This gives auto registration implementations a chance to create an AutoServiceRegistration impl bean.

---
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationAutoConfiguration.java
@@ -0,0 +1,28 @@
+package org.springframework.cloud.client.serviceregistry;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Spencer Gibb
+ */
+@Configuration
+@ConditionalOnBean(AutoServiceRegistrationProperties.class)
+public class AutoServiceRegistrationAutoConfiguration {
+
+	@Autowired(required = false)
+	private AutoServiceRegistration autoServiceRegistration;
+
+	@Autowired
+	private AutoServiceRegistrationProperties properties;
+
+	@PostConstruct
+	protected void init() {
+		if (autoServiceRegistration == null && this.properties.isFailFast()) {
+			throw new IllegalStateException("Auto Service Registration has been requested, but there is no AutoServiceRegistration bean");
+		}
+	}
+}
