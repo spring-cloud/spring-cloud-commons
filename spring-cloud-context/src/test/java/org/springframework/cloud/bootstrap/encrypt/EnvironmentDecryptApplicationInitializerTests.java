f9c6d2b82f888117453bbd3713e5abaf40f76e82
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-02-28 08:57:05 -0800

    Simplified the test with a mock environment

--- spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/encrypt/EnvironmentDecryptApplicationInitializerTests.java
+++ spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/encrypt/EnvironmentDecryptApplicationInitializerTests.java
@@ -129,72 +129,7 @@ public class EnvironmentDecryptApplicationInitializerTests {
 
 
 		ApplicationContext ctxParent = mock(ApplicationContext.class);
-		when(ctxParent.getEnvironment()).thenReturn(new Environment() {
-			@Override
-			public String[] getActiveProfiles() {
-				return new String[0];
-			}
-
-			@Override
-			public String[] getDefaultProfiles() {
-				return new String[0];
-			}
-
-			@Override
-			public boolean acceptsProfiles(String... profiles) {
-				return false;
-			}
-
-			@Override
-			public boolean containsProperty(String key) {
-				return false;
-			}
-
-			@Override
-			public String getProperty(String key) {
-				return null;
-			}
-
-			@Override
-			public String getProperty(String key, String defaultValue) {
-				return null;
-			}
-
-			@Override
-			public <T> T getProperty(String key, Class<T> targetType) {
-				return null;
-			}
-
-			@Override
-			public <T> T getProperty(String key, Class<T> targetType, T defaultValue) {
-				return null;
-			}
-
-			@Override
-			public <T> Class<T> getPropertyAsClass(String key, Class<T> targetType) {
-				return null;
-			}
-
-			@Override
-			public String getRequiredProperty(String key) throws IllegalStateException {
-				return null;
-			}
-
-			@Override
-			public <T> T getRequiredProperty(String key, Class<T> targetType) throws IllegalStateException {
-				return null;
-			}
-
-			@Override
-			public String resolvePlaceholders(String text) {
-				return null;
-			}
-
-			@Override
-			public String resolveRequiredPlaceholders(String text) throws IllegalArgumentException {
-				return null;
-			}
-		});
+		when(ctxParent.getEnvironment()).thenReturn(mock(Environment.class));
 
 		ctx.setParent(ctxParent);
 
