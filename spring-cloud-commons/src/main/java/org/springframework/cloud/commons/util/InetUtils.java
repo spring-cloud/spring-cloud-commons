5a9533a7d513c4fef89a6434386035da42c75228
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-03-01 09:47:22 -0800

    HostInfo is now extensible (#181)
    Fixes gh-151

--- spring-cloud-commons/src/main/java/org/springframework/cloud/commons/util/InetUtils.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/commons/util/InetUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.commons.util;
 
 import java.io.Closeable;
@@ -185,16 +201,16 @@ public class InetUtils implements Closeable {
 	}
 
 	@Data
-	public static final class HostInfo {
+	public static class HostInfo {
 		public boolean override;
 		private String ipAddress;
 		private String hostname;
 
-		HostInfo(String hostname) {
+		public HostInfo(String hostname) {
 			this.hostname = hostname;
 		}
 
-		HostInfo() {
+		public HostInfo() {
 		}
 
 		public int getIpAddressAsInt() {
