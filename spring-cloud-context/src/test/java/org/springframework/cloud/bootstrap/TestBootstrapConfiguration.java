fbe500762ec6c96e0b937fdb026dd919312e094e
Dave Syer <dsyer@pivotal.io>
2017-03-15 11:27:52 +0000

    Apply bootstrap initializers to all contexts in a hierarchy
    The fix for gh-153 was sensible, but has now exposed another issue
    which is that the bootstrap initializers, whilst they are now only
    created once, actually only get applied to the parent context
    in a hierarchy.
    
    Most of the time this doesn't matter because the child contexts all
    benefit from environment changes and additional beans added
    to the parent. If the children are supposed to actually have
    different environments, though, there is a problem. So this change
    locates the existing bootstrap context and applies it to each
    new context created separately.
    
    Unfortunately we have to use reflection to do that for now because
    there is no way to discover the parent with a public API before
    the context is refreshed.
    
    See gh-190

--- spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/TestBootstrapConfiguration.java
+++ spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/TestBootstrapConfiguration.java
@@ -1,6 +1,14 @@
 package org.springframework.cloud.bootstrap;
 
+import java.util.Collections;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 
 import static org.springframework.cloud.bootstrap.TestHigherPriorityBootstrapConfiguration.firstToBeCreated;
 
@@ -8,10 +16,28 @@ import static org.springframework.cloud.bootstrap.TestHigherPriorityBootstrapCon
  * @author Spencer Gibb
  */
 @Order(0)
+@Configuration
 public class TestBootstrapConfiguration {
 
 	public TestBootstrapConfiguration() {
 		firstToBeCreated.compareAndSet(null, TestBootstrapConfiguration.class);
+	}
+
+	@Bean
+	public ApplicationContextInitializer<ConfigurableApplicationContext> customInitializer() {
+		return new ApplicationContextInitializer<ConfigurableApplicationContext>() {
+
+			@Override
+			public void initialize(ConfigurableApplicationContext applicationContext) {
+				ConfigurableEnvironment environment = applicationContext.getEnvironment();
+				environment.getPropertySources()
+						.addLast(new MapPropertySource("customProperties",
+								Collections.<String, Object>singletonMap("custom.foo",
+										environment.resolvePlaceholders(
+												"${spring.application.name:bar}"))));
+			}
+
+		};
 	}
 
 }
