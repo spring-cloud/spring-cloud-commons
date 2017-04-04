0f7e1bb9f06d68d8f257e56bfc6b934db2b46649
Spencer Gibb <spencer@gibb.us>
2017-04-03 19:14:45 -0600

    Adds ServiceRegistry documentation.
    fixes gh-170
    fixes gh-196

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/ServiceRegistry.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/ServiceRegistry.java
@@ -1,19 +1,48 @@
 package org.springframework.cloud.client.serviceregistry;
 
 /**
- * TODO: write javadoc
+ * Contract to register and deregister instances with a Service Registry.
+ *
  * @author Spencer Gibb
+ * @since 1.2.0
  */
 public interface ServiceRegistry<R extends Registration> {
+
+	/**
+	 * Register the registration. Registrations typically have information about
+	 * instances such as: hostname and port.
+	 * @param registration the registraion
+	 */
 	void register(R registration);
 
+	/**
+	 * Deregister the registration.
+	 * @param registration
+	 */
 	void deregister(R registration);
 
+	/**
+	 * Close the ServiceRegistry. This a lifecycle method.
+	 */
 	void close();
 
-	// TODO: return value for success?
+	/**
+	 * Sets the status of the registration. The status values are determined
+	 * by the individual implementations.
+	 *
+	 * @see org.springframework.cloud.client.serviceregistry.endpoint.ServiceRegistryEndpoint
+	 * @param registration the registration to update
+	 * @param status the status to set
+	 */
 	void setStatus(R registration, String status);
 
-	// TODO: concrete return value? Interface?
-	Object getStatus(R registration);
+	/**
+	 * Gets the status of a particular registration.
+	 *
+	 * @see org.springframework.cloud.client.serviceregistry.endpoint.ServiceRegistryEndpoint
+	 * @param registration the registration to query
+	 * @param <T> the type of the status
+	 * @return the status of the registration
+	 */
+	<T> T getStatus(R registration);
 }
