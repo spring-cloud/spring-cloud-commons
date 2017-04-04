930b927723df5898946c6c44b6771c1d26494862
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-04-03 21:17:02 -0500

    Support for config driven Discovery Client
    Making SimpleDiscoveryClient the default, marking NoOpDiscoveryClient as deprecated
    
    fixes gh-180

---
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/simple/SimpleDiscoveryClientAutoConfiguration.java
@@ -0,0 +1,31 @@
+package org.springframework.cloud.client.discovery.simple;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.noop.NoopDiscoveryClientAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Boot Auto-Configuration for Simple Properties based Discovery Client
+ *
+ * @author Biju Kunjummen
+ */
+
+@Configuration
+@ConditionalOnMissingBean(DiscoveryClient.class)
+@EnableConfigurationProperties(SimpleDiscoveryProperties.class)
+@AutoConfigureBefore(NoopDiscoveryClientAutoConfiguration.class)
+public class SimpleDiscoveryClientAutoConfiguration {
+
+	@Autowired
+	private SimpleDiscoveryProperties simpleDiscoveryProperties;
+
+	@Bean
+	public DiscoveryClient simpleDiscoveryClient() {
+		return new SimpleDiscoveryClient(simpleDiscoveryProperties);
+	}
+}
---
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/discovery/simple/SimpleDiscoveryClientPropertiesMappingTests.java
@@ -0,0 +1,88 @@
+package org.springframework.cloud.client.discovery.simple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for mapping properties to instances in {@link SimpleDiscoveryClient}
+ *
+ * @author Biju Kunjummen
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {
+		"spring.cloud.discovery.client.simple.instances.service1[0].uri=http://s1-1:8080",
+		"spring.cloud.discovery.client.simple.instances.service1[1].uri=https://s1-2:8443",
+		"spring.cloud.discovery.client.simple.instances.service2[0].uri=https://s2-1:8080",
+		"spring.cloud.discovery.client.simple.instances.service2[1].uri=https://s2-2:443" })
+public class SimpleDiscoveryClientPropertiesMappingTests {
+
+	@Autowired
+	private SimpleDiscoveryProperties props;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@Test
+	public void propsShouldGetCleanlyMapped() {
+		assertThat(props.getInstances().size()).isEqualTo(2);
+		assertThat(props.getInstances().get("service1").size()).isEqualTo(2);
+		assertThat(props.getInstances().get("service1").get(0).getHost())
+				.isEqualTo("s1-1");
+		assertThat(props.getInstances().get("service1").get(0).getPort()).isEqualTo(8080);
+		assertThat(props.getInstances().get("service1").get(0).getUri())
+				.isEqualTo(URI.create("http://s1-1:8080"));
+		assertThat(props.getInstances().get("service1").get(0).isSecure())
+				.isEqualTo(false);
+
+		assertThat(props.getInstances().get("service2").size()).isEqualTo(2);
+		assertThat(props.getInstances().get("service2").get(0).getHost())
+				.isEqualTo("s2-1");
+		assertThat(props.getInstances().get("service2").get(0).getPort()).isEqualTo(8080);
+		assertThat(props.getInstances().get("service2").get(0).getUri())
+				.isEqualTo(URI.create("https://s2-1:8080"));
+		assertThat(props.getInstances().get("service2").get(0).isSecure())
+				.isEqualTo(true);
+	}
+
+	@Test
+	public void testDiscoveryClientShouldResolveSimpleValues() {
+		assertThat(this.discoveryClient.description())
+				.isEqualTo("Simple Discovery Client");
+		assertThat(this.discoveryClient.getInstances("service1")).hasSize(2);
+
+		ServiceInstance s1 = this.discoveryClient.getInstances("service1").get(0);
+		assertThat(s1.getHost()).isEqualTo("s1-1");
+		assertThat(s1.getPort()).isEqualTo(8080);
+		assertThat(s1.getUri()).isEqualTo(URI.create("http://s1-1:8080"));
+		assertThat(s1.isSecure()).isEqualTo(false);
+	}
+
+	@Test
+	public void testGetServices() {
+		assertThat(this.discoveryClient.getServices())
+				.containsExactlyInAnyOrder("service1", "service2");
+	}
+
+	@Test
+	public void testGetANonExistentServiceShouldReturnAnEmptyList() {
+		assertThat(this.discoveryClient.getInstances("nonexistent")).isNotNull();
+		assertThat(this.discoveryClient.getInstances("nonexistent")).isEmpty();
+	}
+
+	@Configuration
+	@EnableAutoConfiguration
+	public static class SampleConfig {
+	}
+}
