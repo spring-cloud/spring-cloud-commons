ecdf3eebad897f1e62f7341a013ee551404cdd1b
jihor <jihor@yandex.ru>
2017-06-26 19:18:20 +0300

    Add option to control management registration, switch AutoServiceRegistrationProperties to use Lombok

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/AutoServiceRegistrationProperties.java
@@ -2,23 +2,24 @@ package org.springframework.cloud.client.serviceregistry;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Spencer Gibb
  */
 @ConfigurationProperties("spring.cloud.service-registry.auto-registration")
+@Getter
+@Setter
 public class AutoServiceRegistrationProperties {
 
 	/** If Auto-Service Registration is enabled, default to true. */
 	private boolean enabled = true;
 
+	/** Whether to register the management as a service, defaults to true */
+	private boolean registerManagement = true;
+
 	/** Should startup fail if there is no AutoServiceRegistration, default to false. */
 	private boolean failFast = false;
 
-	public boolean isFailFast() {
-		return failFast;
-	}
-
-	public void setFailFast(boolean failFast) {
-		this.failFast = failFast;
-	}
 }
