0b60a49148ee0ea81a5684b933607c04c8b5b40a
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-07-21 18:26:11 -0700

    Show better message for missing config keystore props

--- spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/encrypt/EncryptionBootstrapConfigurationTests.java
+++ spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/encrypt/EncryptionBootstrapConfigurationTests.java
@@ -1,13 +1,19 @@
 package org.springframework.cloud.bootstrap.encrypt;
 
-import static org.junit.Assert.assertEquals;
-
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.security.crypto.encrypt.TextEncryptor;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
 public class EncryptionBootstrapConfigurationTests {
+
+	@Rule
+	public ExpectedException expected = ExpectedException.none();
 
 	@Test
 	public void rsaKeyStore() {
@@ -33,6 +39,25 @@ public class EncryptionBootstrapConfigurationTests {
 		TextEncryptor encryptor = context.getBean(TextEncryptor.class);
 		assertEquals("foo", encryptor.decrypt(encryptor.encrypt("foo")));
 		context.close();
+	}
+
+	@Test
+	public void nonExistentKeystoreLocationShouldNotBeAllowed() {
+		try {
+			new SpringApplicationBuilder(EncryptionBootstrapConfiguration.class)
+					.web(false)
+					.properties("encrypt.key-store.location:classpath:/server.jks1",
+							"encrypt.key-store.password:letmein",
+							"encrypt.key-store.alias:mytestkey",
+							"encrypt.key-store.secret:changeme")
+					.run();
+			assertThat(false)
+					.as("Should not create an application context with invalid keystore location")
+					.isTrue();
+		}
+		catch (Exception e) {
+			assertThat(e).hasRootCauseInstanceOf(IllegalStateException.class);
+		}
 	}
 
 }
