1bd2c3112a07b66752d88f1e7169b57893e26148
Spencer Gibb <spencer@gibb.us>
2017-11-14 11:11:12 -0500

    Make sure cli args are copied to bootstrap.

--- spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/TestBootstrapConfiguration.java
+++ spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/TestBootstrapConfiguration.java
@@ -1,8 +1,12 @@
 package org.springframework.cloud.bootstrap;
 
 import java.util.Collections;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +21,25 @@ import static org.springframework.cloud.bootstrap.TestHigherPriorityBootstrapCon
  */
 @Order(0)
 @Configuration
+@EnableConfigurationProperties
 public class TestBootstrapConfiguration {
 
 	public TestBootstrapConfiguration() {
 		firstToBeCreated.compareAndSet(null, TestBootstrapConfiguration.class);
+	}
+
+	public static List<String> fooSightings = null;
+
+	@Bean
+	@Qualifier("foo-during-bootstrap")
+	public String fooDuringBootstrap(ConfigurableEnvironment environment, ApplicationEventPublisher publisher) {
+		String property = environment.getProperty("test.bootstrap.foo", "undefined");
+
+		if (fooSightings != null) {
+			fooSightings.add(property);
+		}
+
+		return property;
 	}
 
 	@Bean
