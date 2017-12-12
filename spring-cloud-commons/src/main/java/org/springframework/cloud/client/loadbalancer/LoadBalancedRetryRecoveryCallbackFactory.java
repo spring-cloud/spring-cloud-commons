a2047bcc1d18b949ac6dc0aede43e7d0485730ba
saga <spring_holy@163.com>
2017-12-12 17:40:07 +0800

    Add java code comments.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/LoadBalancedRetryRecoveryCallbackFactory.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/LoadBalancedRetryRecoveryCallbackFactory.java
@@ -4,10 +4,16 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.retry.RecoveryCallback;
 
 /**
+ * Factory class to return the implementation of the RecoveryCallback.
  * @author Gang Li
  */
 public interface LoadBalancedRetryRecoveryCallbackFactory {
 
+    /**
+     * Create a RecoveryCallback implementation class.
+     * @param service On behalf of serviceId.
+     * @return A {@link RecoveryCallback} implementation.
+     */
     RecoveryCallback<ClientHttpResponse> createRecoveryCallback(String service);
 
     class NoRecoveryCallbackFactory implements LoadBalancedRetryRecoveryCallbackFactory {
