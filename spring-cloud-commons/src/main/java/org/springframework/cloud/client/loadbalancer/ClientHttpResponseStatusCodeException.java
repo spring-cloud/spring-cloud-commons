84bc3d9bcbad70021822eed81e850e1673b7e5b3
Ryan Baxter <rbaxter@pivotal.io>
2018-01-29 09:15:15 -0500

    Refactoring recovery callback implementation. (#309)

---
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/ClientHttpResponseStatusCodeException.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.client.loadbalancer;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.AbstractClientHttpResponse;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.util.StreamUtils;
+
+/**
+ * {@link RetryableStatusCodeException} that captures a {@link ClientHttpResponse}
+ * @author Ryan Baxter
+ */
+public class ClientHttpResponseStatusCodeException extends RetryableStatusCodeException {
+
+	private ClientHttpResponseWrapper response;
+
+	/**
+	 * Constructor
+	 * @param serviceId The service id
+	 * @param response The response object
+	 * @throws IOException Thrown if the {@link ClientHttpResponse} body cannot be retrieved
+	 */
+	public ClientHttpResponseStatusCodeException(String serviceId, ClientHttpResponse response) throws IOException {
+		super(serviceId, response.getRawStatusCode(), response, null);
+		this.response = new ClientHttpResponseWrapper(response);
+		response.close();
+	}
+
+	@Override
+	public ClientHttpResponse getResponse() {
+		return response;
+	}
+
+	static class ClientHttpResponseWrapper extends AbstractClientHttpResponse {
+
+		private ClientHttpResponse response;
+		private byte[] body;
+
+		public ClientHttpResponseWrapper(ClientHttpResponse response) throws IOException {
+			this.response = response;
+			this.body = StreamUtils.copyToByteArray(response.getBody());
+		}
+
+		@Override
+		public int getRawStatusCode() throws IOException {
+			return response.getRawStatusCode();
+		}
+
+		@Override
+		public String getStatusText() throws IOException {
+			return response.getStatusText();
+		}
+
+		@Override
+		public void close() {
+			response.close();
+		}
+
+		@Override
+		public InputStream getBody() throws IOException {
+			return new ByteArrayInputStream(body);
+		}
+
+		@Override
+		public HttpHeaders getHeaders() {
+			return response.getHeaders();
+		}
+	}
+}
---
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/RibbonRecoveryCallback.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2013-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.client.loadbalancer;
+
+import org.springframework.retry.RecoveryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.RetryException;
+
+import java.net.URI;
+
+/**
+ * An implementation of {@link RecoveryCallback} which relies on an implemtation
+ * of {@link RetryableStatusCodeException} to contain the last response object from
+ * the request
+ * @author LiYuan Lee
+ */
+public abstract class RibbonRecoveryCallback<T, R> implements RecoveryCallback<T> {
+
+	/**
+	 * Create the response returned in the {@link RecoveryCallback}
+	 * @param response The response from the HTTP client
+	 * @param uri The URI the response is from
+	 * @return The response to be returned
+	 */
+	protected abstract T createResponse(R response, URI uri);
+
+	@Override
+	public T recover(RetryContext context) throws Exception {
+		Throwable lastThrowable = context.getLastThrowable();
+		if (lastThrowable != null) {
+			if (lastThrowable instanceof RetryableStatusCodeException) {
+				RetryableStatusCodeException ex = (RetryableStatusCodeException) lastThrowable;
+				return createResponse((R) ex.getResponse(), ex.getUri());
+			} else if (lastThrowable instanceof Exception){
+				throw (Exception)lastThrowable;
+			}
+		}
+		throw new RetryException("Could not recover", lastThrowable);
+	}
+}
