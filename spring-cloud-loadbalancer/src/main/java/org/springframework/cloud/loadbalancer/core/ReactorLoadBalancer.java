1fa513194b07c949f21ab73af40f3ff5464d93f5
Spencer Gibb <spencer@gibb.us>
2018-08-15 18:45:53 -0400

    Adds API and implementation of ReactiveLoadBalancer
    API lives in spring-cloud-commons module and implementation in
    spring-cloud-loadbalancer.

---
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/core/ReactorLoadBalancer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.loadbalancer.core;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer;
+import org.springframework.cloud.client.loadbalancer.reactive.Request;
+import org.springframework.cloud.client.loadbalancer.reactive.Response;
+
+public interface ReactorLoadBalancer<T> extends ReactiveLoadBalancer<T> {
+	/**
+	 * Choose the next server based on the load balancing algorithm
+	 * @param request
+	 * @return
+	 */
+	Mono<Response<T>> choose(Request request);
+
+	default Mono<Response<T>> choose() {
+		return choose(REQUEST);
+	}
+}
---
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/support/LoadBalancerClientFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.loadbalancer.support;
+
+import org.springframework.cloud.context.named.NamedContextFactory;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientConfiguration;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClientSpecification;
+import org.springframework.core.env.Environment;
+
+/**
+ * A factory that creates client, load balancer and client configuration instances. It
+ * creates a Spring ApplicationContext per client name, and extracts the beans that it
+ * needs from there.
+ *
+ * @author Spencer Gibb
+ * @author Dave Syer
+ */
+public class LoadBalancerClientFactory extends NamedContextFactory<LoadBalancerClientSpecification> {
+
+	public static final String NAMESPACE = "loadbalancer";
+	public static final String PROPERTY_NAME = NAMESPACE + ".client.name";
+
+	public LoadBalancerClientFactory() {
+		super(LoadBalancerClientConfiguration.class, NAMESPACE, PROPERTY_NAME);
+	}
+
+	public String getName(Environment environment) {
+		return environment.getProperty(PROPERTY_NAME);
+	}
+
+}
+
