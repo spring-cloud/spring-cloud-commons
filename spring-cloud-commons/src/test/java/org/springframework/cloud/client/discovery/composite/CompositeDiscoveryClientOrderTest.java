e115562cf89aa69a0e91c62510fedd58e819243d
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2018-09-10 09:21:20 +0200

    Fix after code review - add license notice, reformat imports, edit docs.

--- spring-cloud-commons/src/test/java/org/springframework/cloud/client/discovery/composite/CompositeDiscoveryClientOrderTest.java
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/discovery/composite/CompositeDiscoveryClientOrderTest.java
@@ -1,10 +1,20 @@
-package org.springframework.cloud.client.discovery.composite;
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.CUSTOM_DISCOVERY_CLIENT;
-import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.CUSTOM_SERVICE_ID;
-import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.DEFAULT_ORDER_DISCOVERY_CLIENT;
-import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.FOURTH_DISCOVERY_CLIENT;
+package org.springframework.cloud.client.discovery.composite;
 
 import java.util.List;
 
@@ -17,6 +27,12 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.CUSTOM_DISCOVERY_CLIENT;
+import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.CUSTOM_SERVICE_ID;
+import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.DEFAULT_ORDER_DISCOVERY_CLIENT;
+import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.FOURTH_DISCOVERY_CLIENT;
+
 /**
  * Tests for the support of ordered {@link DiscoveryClient} instances in {@link CompositeDiscoveryClient}
  *
@@ -24,7 +40,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = "spring.cloud.discovery.client.simple.order:2", classes = {
-		CompositeDiscoveryClientTestsConfig.class })
+		CompositeDiscoveryClientTestsConfig.class})
 public class CompositeDiscoveryClientOrderTest {
 
 	@Autowired
--- spring-cloud-commons/src/test/java/org/springframework/cloud/client/discovery/composite/CompositeDiscoveryClientTests.java
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/discovery/composite/CompositeDiscoveryClientTests.java
@@ -1,7 +1,20 @@
-package org.springframework.cloud.client.discovery.composite;
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.CUSTOM_SERVICE_ID;
+package org.springframework.cloud.client.discovery.composite;
 
 import java.net.URI;
 
@@ -14,9 +27,12 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.cloud.client.discovery.composite.CompositeDiscoveryClientTestsConfig.CUSTOM_SERVICE_ID;
+
 /**
  * Tests for behavior of Composite Discovery Client
- * 
+ *
  * @author Biju Kunjummen
  */
 
@@ -26,8 +42,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 		"spring.cloud.discovery.client.simple.instances.service1[0].uri=http://s1-1:8080",
 		"spring.cloud.discovery.client.simple.instances.service1[1].uri=https://s1-2:8443",
 		"spring.cloud.discovery.client.simple.instances.service2[0].uri=https://s2-1:8080",
-		"spring.cloud.discovery.client.simple.instances.service2[1].uri=https://s2-2:443", }, classes = {
-				CompositeDiscoveryClientTestsConfig.class })
+		"spring.cloud.discovery.client.simple.instances.service2[1].uri=https://s2-2:443",}, classes = {
+		CompositeDiscoveryClientTestsConfig.class})
 public class CompositeDiscoveryClientTests {
 
 	@Autowired
@@ -45,17 +61,17 @@ public class CompositeDiscoveryClientTests {
 		assertThat(s1.getUri()).isEqualTo(URI.create("http://s1-1:8080"));
 		assertThat(s1.isSecure()).isEqualTo(false);
 	}
-	
+
 	@Test
 	public void getServicesShouldAggregateAllServiceNames() {
 		assertThat(this.discoveryClient.getServices()).containsOnlyOnce("service1", "service2", "custom");
 	}
-	
+
 	@Test
 	public void getDescriptionShouldBeComposite() {
 		assertThat(this.discoveryClient.description()).isEqualTo("Composite Discovery Client");
 	}
-	
+
 	@Test
 	public void getInstancesShouldRespectOrder() {
 		assertThat(this.discoveryClient.getInstances(CUSTOM_SERVICE_ID)).hasSize(1);
