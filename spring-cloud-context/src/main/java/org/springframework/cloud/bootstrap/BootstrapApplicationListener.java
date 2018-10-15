9ac3e8453c1e936e88b95f63ac18b7bca71f4c44
lxy <391861737@qq.com>
2018-10-15 17:34:02 +0800

    Change indents
    I did not notice the code formatting requirements, but now I've changed it.
    
    Fixes gh-425

--- spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/BootstrapApplicationListener.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/BootstrapApplicationListener.java
@@ -173,23 +173,23 @@ public class BootstrapApplicationListener
 				// Don't use the default properties in this builder
 				.registerShutdownHook(false).logStartupInfo(false)
 				.web(WebApplicationType.NONE);
-        final SpringApplication builderApplication = builder.application();
-        if(builderApplication.getMainApplicationClass() == null){
-            // gh-425:
-            // SpringApplication cannot deduce the MainApplicationClass here
-            // if it is booted from SpringBootServletInitializer due to the
-            // absense of the "main" method in stackTraces.
-            // But luckily this method's second parameter "application" here
-            // carries the real MainApplicationClass which has been explicitly
-            // set by SpringBootServletInitializer itself already.
-            builder.main(application.getMainApplicationClass());
-        }
+		final SpringApplication builderApplication = builder.application();
+		if(builderApplication.getMainApplicationClass() == null){
+			// gh-425:
+			// SpringApplication cannot deduce the MainApplicationClass here
+			// if it is booted from SpringBootServletInitializer due to the
+			// absense of the "main" method in stackTraces.
+			// But luckily this method's second parameter "application" here
+			// carries the real MainApplicationClass which has been explicitly
+			// set by SpringBootServletInitializer itself already.
+			builder.main(application.getMainApplicationClass());
+		}
 		if (environment.getPropertySources().contains("refreshArgs")) {
 			// If we are doing a context refresh, really we only want to refresh the
 			// Environment, and there are some toxic listeners (like the
 			// LoggingApplicationListener) that affect global static state, so we need a
 			// way to switch those off.
-            builderApplication
+			builderApplication
 					.setListeners(filterListeners(builderApplication.getListeners()));
 		}
 		List<Class<?>> sources = new ArrayList<>();
