791a8807b5d09853c2a74f83a3e84f3ebc6d92bc
Spencer Gibb <spencer@gibb.us>
2018-10-24 12:07:01 -0400

    Adds test for existence of ManagementServerProperties class.
    fixes gh-435

---
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/discovery/ManagementServerPortUtilsTests.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.client.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.test.ClassPathExclusions;
+import org.springframework.cloud.test.ModifiedClassPathRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(ModifiedClassPathRunner.class)
+@ClassPathExclusions({ "spring-boot-actuator-autoconfigure-*" })
+public class ManagementServerPortUtilsTests {
+
+	@Test
+	public void contextStarts() {
+		try (ConfigurableApplicationContext context = new SpringApplicationBuilder()
+				.web(WebApplicationType.NONE).sources(TestApp.class).run()) {
+
+			assertThat(ManagementServerPortUtils.hasActuator).isFalse();
+		}
+	}
+
+	@SpringBootConfiguration
+	@EnableAutoConfiguration
+	protected static class TestApp {}
+}
