d05dc54366f6c1db55e0627d55062f02440856d4
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-11-15 11:53:11 +0100

    Applied the suggested changes

---
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/configuration/CompatibilityVerifierTests.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.cloud.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.BDDAssertions;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.springframework.boot.test.rule.OutputCapture;
+
+/**
+ * @author Marcin Grzejszczak
+ */
+public class CompatibilityVerifierTests {
+
+	@Rule public OutputCapture outputCapture = new OutputCapture();
+
+	@Test
+	public void should_not_print_the_report_when_no_errors_were_found() {
+		CompositeCompatibilityVerifier verifier = new CompositeCompatibilityVerifier(new ArrayList<CompatibilityVerifier>());
+
+		verifier.verifyDependencies();
+
+		BDDAssertions.then(outputCapture.toString()).doesNotContain("SPRING CLOUD VERIFICATION FAILED");
+	}
+
+	@Test
+	public void should_print_the_report_when_errors_were_found() {
+		List<CompatibilityVerifier> list = new ArrayList<>();
+		list.add(new CompatibilityVerifier() {
+			@Override
+			public VerificationResult verify() {
+				return VerificationResult.notCompatible("Wrong Boot version", "Use Boot version 1.2");
+			}
+		});
+		list.add(new CompatibilityVerifier() {
+			@Override
+			public VerificationResult verify() {
+				return VerificationResult.notCompatible("Wrong JDK version", "Use JDK 25");
+			}
+		});
+		CompositeCompatibilityVerifier verifier = new CompositeCompatibilityVerifier(list);
+
+		try {
+			verifier.verifyDependencies();
+			BDDAssertions.fail("should fail");
+		} catch (CompatibilityNotMetException ex) {
+			BDDAssertions.then(ex.results).hasSize(2);
+		}
+	}
+
+}
--- spring-cloud-commons/src/test/java/org/springframework/cloud/configuration/SpringBootDependencyTests.java
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/configuration/SpringBootDependencyTests.java
@@ -1,4 +1,16 @@
-package org.springframework.cloud.commons.util;
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.springframework.cloud.configuration;
 
 import java.util.Collections;
 import java.util.List;
@@ -26,6 +38,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isEmpty();
+		BDDAssertions.then(verificationResult.action).isEmpty();
 	}
 
 	@Test
@@ -43,6 +56,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isNotEmpty();
+		BDDAssertions.then(verificationResult.action).isNotEmpty();
 	}
 
 	@Test
@@ -60,6 +74,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isNotEmpty();
+		BDDAssertions.then(verificationResult.action).isNotEmpty();
 	}
 
 	@Test
@@ -77,6 +92,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isEmpty();
+		BDDAssertions.then(verificationResult.action).isEmpty();
 	}
 
 	@Test
@@ -90,9 +106,9 @@ public class SpringBootDependencyTests {
 			}
 		};
 		versionVerifier.ACCEPTED_VERSIONS.clear();
-		versionVerifier.ACCEPTED_VERSIONS.put("2.5", new MismatchPredicate() {
+		versionVerifier.ACCEPTED_VERSIONS.put("2.5", new CompatibilityPredicate() {
 			@Override
-			public boolean accept() {
+			public boolean isCompatible() {
 				return true;
 			}
 		});
@@ -100,6 +116,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isEmpty();
+		BDDAssertions.then(verificationResult.action).isEmpty();
 	}
 
 	@Test
@@ -117,6 +134,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isNotEmpty();
+		BDDAssertions.then(verificationResult.action).isNotEmpty();
 	}
 
 	@Test
@@ -129,6 +147,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isEmpty();
+		BDDAssertions.then(verificationResult.action).isEmpty();
 	}
 
 	@Test
@@ -147,6 +166,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isEmpty();
+		BDDAssertions.then(verificationResult.action).isEmpty();
 	}
 
 	@Test
@@ -164,6 +184,7 @@ public class SpringBootDependencyTests {
 		VerificationResult verificationResult = versionVerifier.verify();
 
 		BDDAssertions.then(verificationResult.description).isNotEmpty();
+		BDDAssertions.then(verificationResult.action).isNotEmpty();
 	}
 
 }
