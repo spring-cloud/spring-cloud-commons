b54034d436d80fef518c2cc65d91e19e5fbe2be5
Jasper Aarts <jaarts@siilo.com>
2019-01-28 13:54:44 +1100

    Fix override behaviour when using CompositePropertySource. Fixes gh-471

--- spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/encrypt/EnvironmentDecryptApplicationInitializer.java
+++ spring-cloud-context/src/main/java/org/springframework/cloud/bootstrap/encrypt/EnvironmentDecryptApplicationInitializer.java
@@ -15,12 +15,7 @@
  */
 package org.springframework.cloud.bootstrap.encrypt;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Pattern;
 
 import org.apache.commons.logging.Log;
@@ -184,9 +179,11 @@ public class EnvironmentDecryptApplicationInitializer implements
 			Map<String, Object> overrides) {
 
 		if (source instanceof CompositePropertySource) {
+			List<PropertySource<?>> propertySources = new ArrayList<>(
+					((CompositePropertySource) source).getPropertySources());
+			Collections.reverse(propertySources);
 
-			for (PropertySource<?> nested : ((CompositePropertySource) source)
-					.getPropertySources()) {
+			for (PropertySource<?> nested : propertySources) {
 				collectEncryptedProperties(nested, overrides);
 			}
 
