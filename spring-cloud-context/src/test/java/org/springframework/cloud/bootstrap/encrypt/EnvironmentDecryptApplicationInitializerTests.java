a0c8344633da71f33e740ae4a1d7ed1e4ffdef94
Tim Ysewyn <tysewyn@pivotal.io>
2019-07-26 15:58:13 +0200

    Updated license header

--- spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/encrypt/EnvironmentDecryptApplicationInitializerTests.java
+++ spring-cloud-context/src/test/java/org/springframework/cloud/bootstrap/encrypt/EnvironmentDecryptApplicationInitializerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2013-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -49,6 +49,7 @@ import static org.springframework.cloud.bootstrap.encrypt.EnvironmentDecryptAppl
 /**
  * @author Dave Syer
  * @author Biju Kunjummen
+ * @author Tim Ysewyn
  */
 public class EnvironmentDecryptApplicationInitializerTests {
 
