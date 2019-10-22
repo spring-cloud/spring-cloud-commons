b76712ee4ef012055f39042edb2d9436ecc8953d
Toshiaki Maki <tmaki@pivotal.io>
2019-10-22 18:08:09 +0900

    Add Customizer.once

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/Customizer.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/Customizer.java
@@ -16,13 +16,40 @@
 
 package org.springframework.cloud.client.circuitbreaker;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+
 /**
  * Customizes the parameterized class.
  *
  * @author Ryan Baxter
+ * @author Toshiaki Maki
  */
 public interface Customizer<TOCUSTOMIZE> {
 
 	void customize(TOCUSTOMIZE tocustomize);
+
+	/**
+	 * Create a wrapped customizer that guarantees that the {@link #customize(Object)}
+	 * method of the delegated <code>customizer</code> is called at most once per target.
+	 * @param customizer a customizer to be delegated
+	 * @param keyMapper a mapping function to produce the identifier of the target
+	 * @param <T> the type of the target to customize
+	 * @param <K> the type of the identifier of the target
+	 * @return a wrapped customizer
+	 */
+	static <T, K> Customizer<T> once(Customizer<T> customizer,
+			Function<? super T, ? extends K> keyMapper) {
+		final Set<K> customized = Collections.newSetFromMap(new ConcurrentHashMap<>());
+		return t -> {
+			final K key = keyMapper.apply(t);
+			if (!customized.contains(key)) {
+				customized.add(key);
+				customizer.customize(t);
+			}
+		};
+	}
 
 }
---
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/circuitbreaker/CustomizerTests.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.client.circuitbreaker;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class CustomizerTests {
+
+	@Test
+	public void testCustomizedOnlyOnce() {
+		AtomicInteger counter = new AtomicInteger(0);
+		final Customizer<AtomicInteger> customizer = Customizer
+				.once(AtomicInteger::incrementAndGet, Object::hashCode);
+		customizer.customize(counter);
+		customizer.customize(counter);
+		customizer.customize(counter);
+		Assertions.assertThat(counter.get()).isEqualTo(1);
+
+		AtomicInteger anotherCounter = new AtomicInteger(0);
+		customizer.customize(anotherCounter);
+		customizer.customize(anotherCounter);
+		Assertions.assertThat(counter.get()).isEqualTo(1);
+	}
+
+}
