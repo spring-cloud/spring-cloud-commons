d7767a058af31c6c41f0823c3ada0aa51703a96d
Toshiaki Maki <tmaki@pivotal.io>
2020-01-16 00:19:03 +0900

    Add once method for Customizer of circuit breaker (#626)
    * Add Customizer.once
    
    * Make the logic of Customer.once atomic
    
    * Add document on how to use once method

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/Customizer.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/circuitbreaker/Customizer.java
@@ -16,13 +16,39 @@
 
 package org.springframework.cloud.client.circuitbreaker;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
+
 /**
  * Customizes the parameterized class.
  *
  * @author Ryan Baxter
+ * @author Toshiaki Maki
  */
 public interface Customizer<TOCUSTOMIZE> {
 
 	void customize(TOCUSTOMIZE tocustomize);
+
+	/**
+	 * Create a wrapped customizer that guarantees that the {@link #customize(Object)}
+	 * method of the delegated <code>customizer</code> is called at most once per target.
+	 * @param customizer a customizer to be delegated
+	 * @param keyMapper a mapping function to produce the identifier of the target
+	 * @param <T> the type of the target to customize
+	 * @param <K> the type of the identifier of the target
+	 * @return a wrapped customizer
+	 */
+	static <T, K> Customizer<T> once(Customizer<T> customizer,
+			Function<? super T, ? extends K> keyMapper) {
+		final ConcurrentMap<K, Boolean> customized = new ConcurrentHashMap<>();
+		return t -> {
+			final K key = keyMapper.apply(t);
+			customized.computeIfAbsent(key, k -> {
+				customizer.customize(t);
+				return true;
+			});
+		};
+	}
 
 }
