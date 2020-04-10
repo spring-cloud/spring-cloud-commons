f168c63fd13cae55fbd88e215137997124d3cd53
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2020-04-10 16:27:16 +0200

    Add metrics map to CompletionContext.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/CompletionContext.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/CompletionContext.java
@@ -16,17 +16,41 @@
 
 package org.springframework.cloud.client.loadbalancer.reactive;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.core.style.ToStringCreator;
 
 /**
  * @author Spencer Gibb
+ * @author Olga Maciaszek-Sharma
  */
-// TODO: add metrics
 public class CompletionContext {
 
 	private final Status status;
 
 	private final Throwable throwable;
+
+	private final Map<Object, Object> metrics;
+
+	public CompletionContext(Status status) {
+		this(status, null, new HashMap<>());
+	}
+
+	public CompletionContext(Status status, Throwable throwable) {
+		this(status, throwable, new HashMap<>());
+	}
+
+	public CompletionContext(Status status, Map<Object, Object> metrics) {
+		this(status, null, metrics);
+	}
+
+	public CompletionContext(Status status, Throwable throwable, Map<Object, Object> metrics) {
+		this.status = status;
+		this.throwable = throwable;
+		this.metrics = metrics;
+	}
 
 	public static CompletionContext success() {
 		return new CompletionContext(Status.SUCCESS);
@@ -38,15 +62,6 @@ public class CompletionContext {
 
 	public static CompletionContext failed(Throwable t) {
 		return new CompletionContext(Status.FAILED, t);
-	}
-
-	public CompletionContext(Status status) {
-		this(status, null);
-	}
-
-	public CompletionContext(Status status, Throwable throwable) {
-		this.status = status;
-		this.throwable = throwable;
 	}
 
 	public Status getStatus() {
@@ -62,7 +77,13 @@ public class CompletionContext {
 		ToStringCreator to = new ToStringCreator(this);
 		to.append("status", this.status);
 		to.append("throwable", this.throwable);
+		to.append("metrics", mapToString(metrics));
 		return to.toString();
+	}
+
+	private String mapToString(Map<Object, Object> map) {
+		return map.keySet().stream().map(key -> key + "=" + map.get(String.valueOf(key)))
+				.collect(Collectors.joining(", ", "{", "}"));
 	}
 
 	/**
