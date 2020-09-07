bd5535ffc5592814eabcbe5b08ddd96025d88173
Dave Syer <dsyer@vmware.com>
2020-09-07 15:25:12 +0100

    Make HATEOAS properly optional

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/hypermedia/CloudHypermediaAutoConfiguration.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/hypermedia/CloudHypermediaAutoConfiguration.java
@@ -21,12 +21,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.hypermedia.CloudHypermediaAutoConfiguration.CloudHypermediaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.Link;
 
 /**
  * Registers a default {@link RemoteResourceRefresher} if at least one
@@ -36,6 +38,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Oliver Gierke
  */
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(Link.class)
 @ConditionalOnBean(type = "org.springframework.cloud.client.hypermedia.RemoteResource")
 @EnableConfigurationProperties(CloudHypermediaProperties.class)
 public class CloudHypermediaAutoConfiguration {
@@ -49,8 +52,7 @@ public class CloudHypermediaAutoConfiguration {
 	@Bean
 	@ConditionalOnMissingBean
 	public RemoteResourceRefresher discoveredResourceRefresher() {
-		return new RemoteResourceRefresher(this.discoveredResources,
-				this.properties.getRefresh().getFixedDelay(),
+		return new RemoteResourceRefresher(this.discoveredResources, this.properties.getRefresh().getFixedDelay(),
 				this.properties.getRefresh().getInitialDelay());
 	}
 
