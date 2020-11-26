8976a53dfe37975df5b4847004aefc91723d98cb
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2020-11-26 10:36:22 +0100

    Add docs and javadocs.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/LoadBalancerProperties.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/LoadBalancerProperties.java
@@ -56,12 +56,26 @@ public class LoadBalancerProperties {
 		 */
 		private Duration interval = Duration.ofSeconds(25);
 
+		/**
+		 * Interval for refetching available service instances.
+		 */
 		private Duration refetchInstancesInterval = Duration.ofSeconds(25);
 
 		private Map<String, String> path = new LinkedCaseInsensitiveMap<>();
 
+		/**
+		 * Indicates whether the instances should be refetched by the
+		 * <code>HealthCheckServiceInstanceListSupplier</code>. This can be used if the
+		 * instances can be updated and the underlying delegate does not provide an
+		 * ongoing flux.
+		 */
 		private boolean refetchInstances = false;
 
+		/**
+		 * Indicates whether health checks should keep repeating. It might be useful to
+		 * set it to <code>false</code> if periodically refetching the instances, as every
+		 * refetch will also trigger a healthcheck.
+		 */
 		private boolean repeatHealthCheck = true;
 
 		public boolean getRefetchInstances() {
