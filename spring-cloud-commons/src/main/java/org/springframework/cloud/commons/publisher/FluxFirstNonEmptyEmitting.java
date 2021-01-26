1a22952b0293b8b74f339c7fc9c0acaf927474d1
spencergibb <spencer@gibb.us>
2021-01-26 11:43:00 -0500

    Removes reflection and uses static stepName.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/commons/publisher/FluxFirstNonEmptyEmitting.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/commons/publisher/FluxFirstNonEmptyEmitting.java
@@ -269,12 +269,6 @@ final class FluxFirstNonEmptyEmitting<T> extends Flux<T> implements Scannable, P
 	static final class FirstNonEmptyEmittingSubscriber<T> extends Operators.DeferredSubscription
 			implements CoreSubscriber<T>, Scannable, Subscription {
 
-		private static final Field s = ReflectionUtils.findField(Operators.DeferredSubscription.class, "s",
-				Subscription.class);
-		static {
-			ReflectionUtils.makeAccessible(s);
-		}
-
 		final RaceCoordinator<T> parent;
 
 		final CoreSubscriber<? super T> actual;
@@ -292,22 +286,9 @@ final class FluxFirstNonEmptyEmitting<T> extends Flux<T> implements Scannable, P
 		@Override
 		@Nullable
 		public Object scanUnsafe(Attr key) {
-			if (key == Attr.PARENT) {
-				return ReflectionUtils.getField(s, this);
-			}
-			if (key == Attr.CANCELLED) {
-				return parent.cancelled;
-			}
-
-			return doScanUnsafe(key);
-		}
-
-		@Nullable
-		Object doScanUnsafe(Attr key) {
-			if (key == Attr.ACTUAL) {
-				return actual;
-			}
-			return null;
+			if (key == Attr.ACTUAL) return actual;
+			if (key == Attr.CANCELLED) return parent.cancelled;
+			return super.scanUnsafe(key);
 		}
 
 		@Override
@@ -346,27 +327,7 @@ final class FluxFirstNonEmptyEmitting<T> extends Flux<T> implements Scannable, P
 
 		@Override
 		public String stepName() {
-			// /!\ this code is duplicated in `Scannable#stepName` in order to use
-			// toString instead of simple class name
-
-			/*
-			 * Strip an operator name of various prefixes and suffixes.
-			 *
-			 * @param name the operator name, usually simpleClassName or fully-qualified
-			 * classname.
-			 *
-			 * @return the stripped operator name
-			 */
-			String name = getClass().getSimpleName();
-			if (name.contains("@") && name.contains("$")) {
-				name = name.substring(0, name.indexOf('$')).substring(name.lastIndexOf('.') + 1);
-			}
-			String stripped = OPERATOR_NAME_UNRELATED_WORDS_PATTERN.matcher(name).replaceAll("");
-
-			if (!stripped.isEmpty()) {
-				return stripped.substring(0, 1).toLowerCase() + stripped.substring(1);
-			}
-			return stripped;
+			return "CloudFlux.firstNonEmpty";
 		}
 
 	}
