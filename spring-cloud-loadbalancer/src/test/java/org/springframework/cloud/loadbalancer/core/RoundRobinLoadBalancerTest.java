b208a60957925fdeed8d8e75c2907c22a75d7add
jizhuozhi <jizhuozhi@jd.com>
2022-02-28 21:24:20 +0800

    Enforces the order of RoundRobinLoadBalancer when positive overflow

---
+++ spring-cloud-loadbalancer/src/test/java/org/springframework/cloud/loadbalancer/core/RoundRobinLoadBalancerTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2012-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.loadbalancer.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.loadbalancer.support.SimpleObjectProvider;
+
+import static java.lang.Integer.MAX_VALUE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class RoundRobinLoadBalancerTest {
+
+	@Test
+	void shouldEnforceOrderWhenPositiveOverflow() {
+		List<ServiceInstance> instances = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			ServiceInstance instance = mock(ServiceInstance.class);
+			when(instance.getInstanceId()).thenReturn(i + "");
+			instances.add(instance);
+		}
+		SameInstancePreferenceServiceInstanceListSupplier supplier = mock(
+				SameInstancePreferenceServiceInstanceListSupplier.class);
+		when(supplier.get(any())).thenReturn(Flux.just(instances));
+
+		RoundRobinLoadBalancer loadBalancer = new RoundRobinLoadBalancer(new SimpleObjectProvider<>(supplier),
+				"shouldStartFromZeroWhenPositiveOverflow", MAX_VALUE);
+
+		for (int i = 0; i < 10; i++) {
+			ServiceInstance chosen = loadBalancer.choose().block().getServer();
+			assertThat(chosen.getInstanceId()).isEqualTo(i + "");
+		}
+	}
+}
