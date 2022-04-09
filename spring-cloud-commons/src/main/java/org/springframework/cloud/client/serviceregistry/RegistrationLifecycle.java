b8d74d49e3e6847e45f73b93eae58e651135f787
huifer <huifer97@163.com>
2022-04-09 12:19:13 +0800

    check style

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/RegistrationLifecycle.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/serviceregistry/RegistrationLifecycle.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.client.serviceregistry;
 
 import org.springframework.core.Ordered;
@@ -9,6 +25,9 @@ import org.springframework.core.Ordered;
  */
 public interface RegistrationLifecycle<R extends Registration> extends Ordered {
 
+	/**
+	 * default order.
+	 */
 	int DEFAULT_ORDER = 0;
 
 	/**
--- spring-cloud-commons/src/test/java/org/springframework/cloud/client/serviceregistry/AbstractAutoServiceRegistrationRegistrationLifecycleTests.java
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/serviceregistry/AbstractAutoServiceRegistrationRegistrationLifecycleTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.client.serviceregistry;
 
 import java.net.URI;
@@ -7,10 +23,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,6 +31,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * AbstractAutoServiceRegistrationRegistrationLifecycleTests
@@ -44,8 +59,6 @@ public class AbstractAutoServiceRegistrationRegistrationLifecycleTests {
 	public static class RegistrationLifecycleImpl
 			implements RegistrationLifecycle<TestRegistrationLifecycleRegistration> {
 
-		private static final Logger log = LoggerFactory.getLogger(RegistrationLifecycleImpl.class);
-
 		@Override
 		public void postProcessBeforeStartRegister(TestRegistrationLifecycleRegistration registration) {
 			registration.getMetadata().putIfAbsent("test_data", "test_data");
@@ -54,24 +67,26 @@ public class AbstractAutoServiceRegistrationRegistrationLifecycleTests {
 
 		@Override
 		public void postProcessAfterStartRegister(TestRegistrationLifecycleRegistration registration) {
-			Assert.assertTrue(registration.getMetadata().containsKey("test_data"));
-			Assert.assertEquals(registration.getMetadata().get("test_data"), "test_data");
-			Assert.assertTrue(registration.getMetadata().containsKey("is_start"));
+			assertThat(registration.getMetadata()).containsKey("test_data");
+			assertThat(registration.getMetadata().get("test_data")).isEqualTo("test_data");
+			assertThat(registration.getMetadata()).containsKey("is_start");
 			registration.getMetadata().putIfAbsent("is_start", "true");
 		}
 
 		@Override
 		public void postProcessBeforeStopRegister(TestRegistrationLifecycleRegistration registration) {
-			Assert.assertEquals(registration.getMetadata().get("is_start"), "true");
-			registration.getMetadata().putIfAbsent("is_stop", "false");
-			registration.getMetadata().putIfAbsent("is_start", "false");
+			assertThat(registration.getMetadata().get("is_start")).isEqualTo("true");
+			Map<String, String> metadata = registration.getMetadata();
+			metadata.putIfAbsent("is_stop", "false");
+			metadata.putIfAbsent("is_start", "false");
 		}
 
 		@Override
 		public void postProcessAfterStopRegister(TestRegistrationLifecycleRegistration registration) {
-			Assert.assertEquals(registration.getMetadata().get("test_data"), "test_data");
-			Assert.assertEquals(registration.getMetadata().get("is_stop"), "false");
-			Assert.assertEquals(registration.getMetadata().get("is_start"), "false");
+
+			assertThat(registration.getMetadata().get("test_data")).isEqualTo("test_data");
+			assertThat(registration.getMetadata().get("is_stop")).isEqualTo("false");
+			assertThat(registration.getMetadata().get("is_start")).isEqualTo("false");
 			registration.getMetadata().putIfAbsent("is_stop", "true");
 		}
 
@@ -81,14 +96,12 @@ public class AbstractAutoServiceRegistrationRegistrationLifecycleTests {
 	@Configuration(proxyBeanMethods = false)
 	public static class Config {
 
-		@Bean(value = "testAutoServiceRegistration")
+		@Bean
 		public TestAutoServiceRegistrationLifecycle testAutoServiceRegistration(
 				AutoServiceRegistrationProperties properties) {
 			List<RegistrationLifecycle<TestRegistrationLifecycleRegistration>> registrationLifecycles = new ArrayList<>();
 			registrationLifecycles.add(new RegistrationLifecycleImpl());
-			TestAutoServiceRegistrationLifecycle testAutoServiceRegistrationLifecycle = new TestAutoServiceRegistrationLifecycle(
-					properties, registrationLifecycles);
-			return testAutoServiceRegistrationLifecycle;
+			return new TestAutoServiceRegistrationLifecycle(properties, registrationLifecycles);
 		}
 
 	}
