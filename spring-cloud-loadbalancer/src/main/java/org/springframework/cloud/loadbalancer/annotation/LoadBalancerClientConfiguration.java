366c5f52bd008ad7a95fe8714ef4586f4f6d3da4
Olga Maciaszek-Sharma <olga.maciaszek@gmail.com>
2023-02-22 11:07:13 +0100

    Add caching to all user-enabled configs other than healthcheck lb. (#1208)

--- spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/annotation/LoadBalancerClientConfiguration.java
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/annotation/LoadBalancerClientConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -120,7 +120,7 @@ public class LoadBalancerClientConfiguration {
 		public ServiceInstanceListSupplier requestBasedStickySessionDiscoveryClientServiceInstanceListSupplier(
 				ConfigurableApplicationContext context) {
 			return ServiceInstanceListSupplier.builder().withDiscoveryClient().withRequestBasedStickySession()
-					.build(context);
+					.withCaching().build(context);
 		}
 
 		@Bean
@@ -130,7 +130,7 @@ public class LoadBalancerClientConfiguration {
 		public ServiceInstanceListSupplier sameInstancePreferenceServiceInstanceListSupplier(
 				ConfigurableApplicationContext context) {
 			return ServiceInstanceListSupplier.builder().withDiscoveryClient().withSameInstancePreference()
-					.build(context);
+					.withCaching().build(context);
 		}
 
 	}
@@ -176,7 +176,7 @@ public class LoadBalancerClientConfiguration {
 		public ServiceInstanceListSupplier requestBasedStickySessionDiscoveryClientServiceInstanceListSupplier(
 				ConfigurableApplicationContext context) {
 			return ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().withRequestBasedStickySession()
-					.build(context);
+					.withCaching().build(context);
 		}
 
 		@Bean
@@ -186,7 +186,7 @@ public class LoadBalancerClientConfiguration {
 		public ServiceInstanceListSupplier sameInstancePreferenceServiceInstanceListSupplier(
 				ConfigurableApplicationContext context) {
 			return ServiceInstanceListSupplier.builder().withBlockingDiscoveryClient().withSameInstancePreference()
-					.build(context);
+					.withCaching().build(context);
 		}
 
 	}
