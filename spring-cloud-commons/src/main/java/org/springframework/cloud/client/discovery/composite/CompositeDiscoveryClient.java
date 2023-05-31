434fd31a49901185d511bd7c10ec407a1fc6a3d4
sean.ruffatti <sean.ruffatti@uline.com>
2023-05-31 13:01:29 -0500

    renamed tests, extended copyright dates, and added author tags

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/composite/CompositeDiscoveryClient.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/discovery/composite/CompositeDiscoveryClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,7 @@ import org.springframework.core.annotation.AnnotationAwareOrderComparator;
  *
  * @author Biju Kunjummen
  * @author Olga Maciaszek-Sharma
+ * @author Sean Ruffatti
  */
 public class CompositeDiscoveryClient implements DiscoveryClient {
 
@@ -76,7 +77,7 @@ public class CompositeDiscoveryClient implements DiscoveryClient {
 	@Override
 	public void probe() {
 		if (this.discoveryClients != null) {
-			for (DiscoveryClient discoveryClient: this.discoveryClients) {
+			for (DiscoveryClient discoveryClient : this.discoveryClients) {
 				discoveryClient.probe();
 			}
 		}
