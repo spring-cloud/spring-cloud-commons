9472b6fa0c70016ef6da3799605f0c2bd7bb6c5a
jizhuozhi <jizhuozhi.george@gmail.com>
2023-10-10 19:26:47 +0800

    feat(lb): deterministic subsetting algorithm to limit the number of instances

---
+++ spring-cloud-loadbalancer/src/main/java/org/springframework/cloud/loadbalancer/core/SubsetServiceInstanceListSupplier.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.loadbalancer.core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.client.ServiceInstance;
+
+/**
+ * A {@link ServiceInstanceListSupplier} implementation that uses
+ * <a href="https://sre.google/sre-book/load-balancing-datacenter/">deterministic
+ * subsetting</a> to limit the number of instances provided by delegate.
+ *
+ * @author Zhuozhi Ji
+ */
+public class SubsetServiceInstanceListSupplier extends DelegatingServiceInstanceListSupplier {
+
+	static final int DEFAULT_SUBSET_SIZE = 1000;
+
+	final String instanceId;
+
+	final int subsetSize;
+
+	public SubsetServiceInstanceListSupplier(ServiceInstanceListSupplier delegate) {
+		this(delegate, DEFAULT_SUBSET_SIZE);
+	}
+
+	public SubsetServiceInstanceListSupplier(ServiceInstanceListSupplier delegate, int subsetSize) {
+		this(delegate, UUID.randomUUID().toString(), subsetSize);
+	}
+
+	public SubsetServiceInstanceListSupplier(ServiceInstanceListSupplier delegate, String instanceId, int subsetSize) {
+		super(delegate);
+		this.instanceId = instanceId;
+		this.subsetSize = subsetSize;
+	}
+
+	@Override
+	public Flux<List<ServiceInstance>> get() {
+		return delegate.get().map(instances -> {
+			if (instances.isEmpty()) {
+				return instances;
+			}
+
+			instances = new ArrayList<>(instances);
+
+			int clientId = instanceId.hashCode() & Integer.MAX_VALUE;
+			int subsetCount = instances.size() / subsetSize;
+			int round = clientId / subsetCount;
+
+			Random random = new Random(round);
+			Collections.shuffle(instances, random);
+
+			int subsetId = clientId % subsetCount;
+			int start = subsetId * subsetSize;
+			return instances.subList(start, start + subsetSize);
+		});
+	}
+
+}
---
+++ spring-cloud-loadbalancer/src/test/java/org/springframework/cloud/loadbalancer/core/SubsetServiceInstanceListSupplierTest.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.loadbalancer.core;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import org.springframework.cloud.client.DefaultServiceInstance;
+import org.springframework.cloud.client.ServiceInstance;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class SubsetServiceInstanceListSupplierTest {
+
+	private final DiscoveryClientServiceInstanceListSupplier delegate = mock(
+			DiscoveryClientServiceInstanceListSupplier.class);
+
+	@Test
+	void shouldConstructCorrect() {
+		SubsetServiceInstanceListSupplier supplier = new SubsetServiceInstanceListSupplier(delegate);
+		assertThat(supplier.instanceId).isNotNull();
+		assertThat(supplier.subsetSize).isEqualTo(SubsetServiceInstanceListSupplier.DEFAULT_SUBSET_SIZE);
+
+		supplier = new SubsetServiceInstanceListSupplier(delegate, 11);
+		assertThat(supplier.instanceId).isNotNull();
+		assertThat(supplier.subsetSize).isEqualTo(11);
+
+		supplier = new SubsetServiceInstanceListSupplier(delegate, "foobar", 11);
+		assertThat(supplier.instanceId).isEqualTo("foobar");
+		assertThat(supplier.subsetSize).isEqualTo(11);
+	}
+
+	@Test
+	void shouldReturnEmptyWhenDelegateReturnedEmpty() {
+		when(delegate.get()).thenReturn(Flux.just(Collections.emptyList()));
+		SubsetServiceInstanceListSupplier supplier = new SubsetServiceInstanceListSupplier(delegate);
+
+		List<ServiceInstance> serviceInstances = Objects.requireNonNull(supplier.get().blockFirst());
+		assertThat(serviceInstances).isEmpty();
+	}
+
+	@Test
+	void shouldReturnSublistWithGivenSubsetSize() {
+		List<ServiceInstance> instances = IntStream.range(0, 101)
+				.mapToObj(i -> new DefaultServiceInstance(Integer.toString(i), "test", "host" + i, 8080, false, null))
+				.collect(Collectors.toList());
+
+		when(delegate.get()).thenReturn(Flux.just(instances));
+		SubsetServiceInstanceListSupplier supplier = new SubsetServiceInstanceListSupplier(delegate, 5);
+
+		List<ServiceInstance> serviceInstances = Objects.requireNonNull(supplier.get().blockFirst());
+		assertThat(serviceInstances).hasSize(5);
+	}
+
+	@Test
+	void shouldReturnSameSublistForSameInstanceId() {
+		List<ServiceInstance> instances = IntStream.range(0, 101)
+				.mapToObj(i -> new DefaultServiceInstance(Integer.toString(i), "test", "host" + i, 8080, false, null))
+				.collect(Collectors.toList());
+
+		when(delegate.get()).thenReturn(Flux.just(instances));
+
+		SubsetServiceInstanceListSupplier supplier1 = new SubsetServiceInstanceListSupplier(delegate, "foobar", 5);
+		List<ServiceInstance> serviceInstances1 = Objects.requireNonNull(supplier1.get().blockFirst());
+
+		SubsetServiceInstanceListSupplier supplier2 = new SubsetServiceInstanceListSupplier(delegate, "foobar", 5);
+		List<ServiceInstance> serviceInstances2 = Objects.requireNonNull(supplier2.get().blockFirst());
+
+		assertThat(serviceInstances1).isEqualTo(serviceInstances2);
+	}
+
+	@Test
+	void shouldReturnDifferentSublistForDifferentInstanceId() {
+		List<ServiceInstance> instances = IntStream.range(0, 101)
+				.mapToObj(i -> new DefaultServiceInstance(Integer.toString(i), "test", "host" + i, 8080, false, null))
+				.collect(Collectors.toList());
+
+		when(delegate.get()).thenReturn(Flux.just(instances));
+
+		SubsetServiceInstanceListSupplier supplier1 = new SubsetServiceInstanceListSupplier(delegate, "foobar1", 5);
+		List<ServiceInstance> serviceInstances1 = Objects.requireNonNull(supplier1.get().blockFirst());
+
+		SubsetServiceInstanceListSupplier supplier2 = new SubsetServiceInstanceListSupplier(delegate, "foobar2", 5);
+		List<ServiceInstance> serviceInstances2 = Objects.requireNonNull(supplier2.get().blockFirst());
+
+		assertThat(serviceInstances1).isNotEqualTo(serviceInstances2);
+	}
+
+}
