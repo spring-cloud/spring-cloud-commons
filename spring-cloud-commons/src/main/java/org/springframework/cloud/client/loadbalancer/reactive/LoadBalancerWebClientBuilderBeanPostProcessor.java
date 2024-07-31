f92bdf7df5d8523e71039519ae4bc00f617ffdd0
Olga Maciaszek-Sharma <omaciaszeksh@vmware.com>
2024-07-31 17:05:27 +0200

    Reapply "Use ObjectProvider for DeferringLoadBalancerExchangeFilterFunction."
    This reverts commit c1b08f8b9bcb9a762218b78b9416f6b7ab3fe31a.

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/LoadBalancerWebClientBuilderBeanPostProcessor.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/reactive/LoadBalancerWebClientBuilderBeanPostProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +17,10 @@
 package org.springframework.cloud.client.loadbalancer.reactive;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.SimpleObjectProvider;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -30,15 +32,28 @@ import org.springframework.web.reactive.function.client.WebClient;
  * @author Olga Maciaszek-Sharma
  * @since 2.2.0
  */
+@SuppressWarnings({ "removal", "rawtypes" })
 public class LoadBalancerWebClientBuilderBeanPostProcessor implements BeanPostProcessor {
 
-	private final DeferringLoadBalancerExchangeFilterFunction exchangeFilterFunction;
+	private final ObjectProvider<DeferringLoadBalancerExchangeFilterFunction> exchangeFilterFunctionObjectProvider;
 
 	private final ApplicationContext context;
 
+	/**
+	 * @deprecated in favour of
+	 * {@link LoadBalancerWebClientBuilderBeanPostProcessor#LoadBalancerWebClientBuilderBeanPostProcessor(ObjectProvider, ApplicationContext)}
+	 */
+	@Deprecated(forRemoval = true)
 	public LoadBalancerWebClientBuilderBeanPostProcessor(
 			DeferringLoadBalancerExchangeFilterFunction exchangeFilterFunction, ApplicationContext context) {
-		this.exchangeFilterFunction = exchangeFilterFunction;
+		this.exchangeFilterFunctionObjectProvider = new SimpleObjectProvider<>(exchangeFilterFunction);
+		this.context = context;
+	}
+
+	public LoadBalancerWebClientBuilderBeanPostProcessor(
+			ObjectProvider<DeferringLoadBalancerExchangeFilterFunction> exchangeFilterFunction,
+			ApplicationContext context) {
+		this.exchangeFilterFunctionObjectProvider = exchangeFilterFunction;
 		this.context = context;
 	}
 
@@ -48,7 +63,12 @@ public class LoadBalancerWebClientBuilderBeanPostProcessor implements BeanPostPr
 			if (context.findAnnotationOnBean(beanName, LoadBalanced.class) == null) {
 				return bean;
 			}
-			((WebClient.Builder) bean).filter(exchangeFilterFunction);
+			DeferringLoadBalancerExchangeFilterFunction exchangeFilterFunction = exchangeFilterFunctionObjectProvider
+				.getIfAvailable();
+			if (exchangeFilterFunction == null) {
+				throw new IllegalStateException("LoadBalancerExchangeFilterFunction not found");
+			}
+			((WebClient.Builder) bean).filter(exchangeFilterFunctionObjectProvider.getIfAvailable());
 		}
 		return bean;
 	}
