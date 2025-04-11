10ed7213ecb0210f4ef8cb86e94fcf80b3f5a104
Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>
2025-04-11 15:50:10 +0200

    Fix checkstyle. Revert debug changes. Refactor.
    Signed-off-by: Olga Maciaszek-Sharma <olga.maciaszek-sharma@broadcom.com>

--- spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/RequestData.java
+++ spring-cloud-commons/src/main/java/org/springframework/cloud/client/loadbalancer/RequestData.java
@@ -19,7 +19,6 @@ package org.springframework.cloud.client.loadbalancer;
 import java.net.URI;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
