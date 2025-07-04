ed77266df1fadad5f72543bb839ec6b1186aad0e
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2025-07-04 18:49:46 +0700

    Fix typos
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- spring-cloud-commons/src/test/java/org/springframework/cloud/client/hypermedia/CloudHypermediaAutoConfigurationIntegrationTests.java
+++ spring-cloud-commons/src/test/java/org/springframework/cloud/client/hypermedia/CloudHypermediaAutoConfigurationIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,7 @@ public class CloudHypermediaAutoConfigurationIntegrationTests {
 	}
 
 	@Test
-	public void doesNotCreateCloudHypermediaPropertiesifNotActive() {
+	public void doesNotCreateCloudHypermediaPropertiesIfNotActive() {
 
 		try (ConfigurableApplicationContext context = getApplicationContext(Config.class)) {
 			then(context.getBeanNamesForType(CloudHypermediaProperties.class)).hasSize(0);
@@ -71,7 +71,7 @@ public class CloudHypermediaAutoConfigurationIntegrationTests {
 	}
 
 	@Test
-	public void registersResourceRefresherIfDiscoverredResourceIsDefined() {
+	public void registersResourceRefresherIfDiscoveredResourceIsDefined() {
 
 		try (ConfigurableApplicationContext context = getApplicationContext(ConfigWithRemoteResource.class)) {
 
